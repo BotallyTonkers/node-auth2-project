71949db9d113d71807da6ec92a8fdcd466d12aff
Amanda Young <botallytonkers@gmail.com>
2023-11-29 15:50:22 -0700

    fin

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,4 +1,5 @@
 const db = require('../../data/db-config.js');
+// const { unsubscribe } = require('./users-router.js');
 
 function find() {
   /**
@@ -18,6 +19,9 @@ function find() {
       }
     ]
    */
+  return db('users')
+  .join('roles', 'users.role_id', 'roles.role_id')
+  .select('user_id', 'username', 'role_name')
 }
 
 function findBy(filter) {
@@ -34,6 +38,10 @@ function findBy(filter) {
       }
     ]
    */
+    return db('users')
+      .join('roles', 'users.role_id', 'roles.role_id')
+      .select('user_id', 'username', 'password', 'role_name')
+      .where(filter)
 }
 
 function findById(user_id) {
@@ -47,6 +55,10 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+    return db('users')
+      .join('roles', 'users.role_id', 'roles.role_id')
+      .select('user_id', 'username', 'password', 'role_name')
+      .where('users.user_id', user_id).first()
 }
 
 /**
